360a6fef66388a87568ba820ae60726a8e80fa2c
Augusto Teixeira <augustoh.teixeira@gmail.com>
2013-10-30 13:21:26 -0200

    create theme html template

---
+++ diazotheme.mytheme/diazotheme/mytheme/diazo_resources/static/application.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.nav li a').click(function(e) {
+      var $this = $(this);
+      if (!$this.hasClass('active')) {
+        $this.addClass('active');
+      }
+      e.preventDefault();
+    });
+});
